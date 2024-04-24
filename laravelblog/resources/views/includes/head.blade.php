75ab33582cd3fd1e101b9364ba0e2d52ee1a5eb0
iladsuma <67311820+iladsuma@users.noreply.github.com>
2024-04-24 10:01:37 +0700

    routing

---
+++ laravelblog/resources/views/includes/head.blade.php
@@ -0,0 +1,21 @@
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  {!! SEO::generate(true) !!}
+
+  <!-- Favicons -->
+  <link href="{{ asset('images/favicon.ico') }}" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset("css/artikel_v2/vendor/bootstrap.min.css") }}" rel="stylesheet">
+  <link href="{{ asset("css/artikel_v2/vendor/aos.css") }}" rel="stylesheet">
+  <link href="{{ asset('css/artikel_v2/vendor/bootstrap-icons.min.css') }}" rel="stylesheet" />
+  
+  <!-- Template Main CSS File -->
+  <link href="{{ asset("css/artikel_v2/main.css") }}" rel="stylesheet">
--- laravelblog/routes/web.php
+++ laravelblog/routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\Category\CategoryController;
+use App\Http\Controllers\Admin\Comment\CommentsController;
+use App\Http\Controllers\Admin\Dashboard\DashboardController;
+use App\Http\Controllers\Admin\Post\PostsController;
+use App\Http\Controllers\Admin\Post\PreviewController;
+use App\Http\Controllers\Admin\User\LoginHistoryController;
+use App\Http\Controllers\Admin\User\UserController;
+use App\Http\Controllers\Home\Blogs\PostController;
+use App\Http\Controllers\Home\Blogs\SearchController;
+use App\Http\Controllers\Home\CategoryListController;
+use App\Http\Controllers\Home\HomeController;
+use App\Http\Controllers\Home\TagsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +26,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+
+Route::get('/blog/{id}', [PostController::class, 'show'])->name('post');
+Route::post('/blog/{id}', [PostController::class, 'storeComment'])->name('store-comment');
+
+Route::get('/blog/user/{slug}', [PostController::class, 'postByUser'])->name('post-by-user');
+
+Route::get('/category/{category}', [CategoryListController::class, 'show'])->name('categories');
+
+Route::get('/tag/{tag}', [TagsController::class, 'index'])->name('post-by-tag');
+
+Route::middleware(['auth', 'verified', 'role:admin,writter'])->prefix('admin')->group(function () {
+	Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+	Route::resources([
+		'article' => PostsController::class,
+		'comment' => CommentsController::class,
+	]);
+
+	Route::get('/article/p/{id}', [PreviewController::class, 'preview'])->name('preview');
+
+	Route::post("/logout", [LoginController::class, 'logout'])->name('logout');
 });
+
+Route::middleware(['auth', 'verified', 'role:admin'])->prefix('admin')->group(function () {
+	Route::resources([
+		'category' => CategoryController::class,
+		'user' => UserController::class
+	]);
+
+	Route::controller(LoginHistoryController::class)->group(function () {
+		Route::get('/login-history', 'index')->name('login-history.index');
+		Route::delete('/login-history', 'destroy')->name('login-history.destroy');
+	});
+});
+
+require __DIR__ . '/auth.php';
