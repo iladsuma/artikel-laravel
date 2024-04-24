256ae9be7ee995c5ebcade6b86f32e6797b88a29
iladsuma <67311820+iladsuma@users.noreply.github.com>
2024-04-24 09:57:44 +0700

    add validate

---
+++ laravelblog/resources/views/errors/429.branch.php
@@ -0,0 +1,7 @@
+@extends('errors::layout')
+
+@section('title', __('Too Many Requests'))
+
+@section('code', '429')
+
+@section('message', __("We've received too many requests from your IP address. Please wait a moment and try again later."))
